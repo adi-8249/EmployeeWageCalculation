643662ed2fc13ae3b3c0454676b50512727f5a34
Anjaneya <anjaneyaprasad9437@gmail.com>
2021-05-27 22:10:41 +0530

    [Anjaneya]refactor:Welcome Employee wage calculation

---
+++ EmployeeWagecalculation/EmployeeWagecalculation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWagecalculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Calculation");
+        }
+    }
+}
