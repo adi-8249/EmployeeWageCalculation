dc373a28371abbaa09aef78589a1d42a7f9497e1
Anjaneya <anjaneyaprasad9437@gmail.com>
2021-06-07 16:57:05 +0530

    [Anjaneya]Added:Save total wage for each company

--- EmployeeWagecalculation/EmployeeWagecalculation/Program.cs
+++ EmployeeWagecalculation/EmployeeWagecalculation/Program.cs
@@ -7,16 +7,16 @@ namespace EmployeeWagecalculation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Calculation");
-            EmployeeWage employeeWage = new EmployeeWage();
-            employeeWage.CalculateEmployeeAttendance();
-            employeeWage.CalculateDaillyEmpWage();
-            employeeWage.PartTimeWage();
-            employeeWage.EmpWageinswitch();
-            employeeWage.EmpWagePerMonth();
-            employeeWage.EmpWageinCondition();
-            EmployeeWage.CalculationEmpWage();
-            EmployeeWage.EmpWageMultipleCompany("Dmart", 20, 2, 10);
-            EmployeeWage.EmpWageMultipleCompany("Relience", 10,4,20);
+            EmployeeWage dmart = new EmployeeWage("Dmart", 20, 2, 10);
+            EmployeeWage relience = new EmployeeWage("Relience", 10, 4, 20);
+            dmart.empWagecompute();
+            Console.WriteLine(dmart.toString());
+            relience.empWagecompute();
+            Console.WriteLine(relience.toString());
+
+            
+
+
 
 
 
